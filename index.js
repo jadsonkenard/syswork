31c853e135fbe427acfa50d23c2afd14b186e6d8
Jadson <jadsonkenard@gmail.com>
2025-10-21 17:26:31 -0300

    Feat: faz a instalação do sequelize e cria a primeira conexão com a base de dados

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Servidor Node iniciado!");
+import sequelize from "./src/config/database.js";
+
+try {
+  await sequelize.authenticate();
+  console.log("Conexão com o banco estabelecida com sucesso!");
+} catch (error) {
+  console.error("Erro ao conectar no banco:", error);
+}
