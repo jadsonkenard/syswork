bdb84756fa30a244a50e86350512b870375be7a1
Jadson <jadsonkenard@gmail.com>
2025-11-12 18:04:33 -0300

    Feat: Adiciona controle de acesso para as rotas de User

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,13 +4,13 @@ import { authorizeAdmin } from "../middlewares/authorizeAdmin.js";
 
 const router = express.Router();
 
-router.get("/", UserController.getAll);
+router.get("/", authorizeAdmin, UserController.getAll);
 router.post("/", authorizeAdmin, UserController.store);
-router.get("/:id", UserController.findById);
-router.put("/:id/update", UserController.update);
-router.put("/:id/update/password", UserController.updatePassword);
-router.delete("/:id/delete", UserController.delete);
-router.get("/positions/:id", UserController.getUserByPosition);
-router.get("/department/:id", UserController.getUserByDepartment);
+router.get("/:id", authorizeAdmin, UserController.findById);
+router.put("/:id/update", authorizeAdmin, UserController.update);
+router.put("/:id/update/password", authorizeAdmin, UserController.updatePassword);
+router.delete("/:id/delete", authorizeAdmin, UserController.delete);
+router.get("/positions/:id", authorizeAdmin, UserController.getUserByPosition);
+router.get("/department/:id", authorizeAdmin, UserController.getUserByDepartment);
 
 export default router;
