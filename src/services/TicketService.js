abe264297a13d8ccfa9bbb85416619af10912b33
Jadson <jadsonkenard@gmail.com>
2025-12-18 12:52:15 -0300

    Refactor: Ajusta nensagem de erro nó método de alterar o status do chamado

--- src/services/TicketService.js
+++ src/services/TicketService.js
@@ -255,9 +255,7 @@ class TicketService {
         "Chamados finalizados não podem mais sofrer alterações de status."
       );
     } else if (ticket.status === "done" && options.isAdmin) {
-      throw new Error(
-        "Para modificar o status vá até: Gerenciamento de Unidade > Chamados"
-      );
+      throw new Error("Para modificar o status vá até: vá em Editar");
     }
 
     const validStatus = ["open", "in progress", "done"];
